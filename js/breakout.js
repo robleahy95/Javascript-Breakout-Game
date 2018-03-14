5430ebebb7e9a3c0e9763122e6c8aa767accc0e6
robleahy95 <35769579+robleahy95@users.noreply.github.com>
2018-03-14 13:52:42 +0000

    Add files via upload
    bricks dissapear upon collision

--- js/breakout.js
+++ js/breakout.js
@@ -20,6 +20,9 @@ var leftPressed = false;
 var dx = 2;
 var dy = -2;
 
+//counting the score
+var score = 0;
+
 //Ball colour
 var ballColour = "#0095DD";
 
@@ -45,25 +48,31 @@ for(c=0; c<brickColumnCount; c++) {
 function drawBricks() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
-			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "#0095DD";
-			ctx.fill();
-			ctx.closePath;
+			if(bricks[c] [r].status == 1) {
+				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath;
+			}
 		}
 	}
 }
 
+//collisionDetection
 function collisionDetection() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
 			var b = bricks[c][r];
-			if(x > b.x && x < b.x+brickWidth && y> b.y && y < b.y+brickHeight) {
-				dy=-dy;
+			if(b.status == 1) {
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+					dy = -dy; 
+					b.status = 0;
+				}
 			}
 		}
 	}
